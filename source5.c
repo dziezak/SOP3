55319ee2c8e0732b428a0a2e09fa77c0449579a5
dziezak <01189082@pw.edu.pl>
2024-12-21 16:00:28 +0100

    source5 ok

--- source5.c
+++ source5.c
@@ -7,8 +7,10 @@
 
 
 int counter = 0;
-pthread_mutex_t counter_lock;
+pthread_mutex_t counter_lock0;
+pthread_rwlock_t counter_lock;
 
+//wrong reader and writer
 void *reader_thread0(void *arg) {
     pthread_t thread_id = pthread_self();
     printf("{%d} Start reader.\n", thread_id);
@@ -31,19 +33,27 @@ void *writer_thread0(void *arg) {
     printf("{%d} Written %d.\n", thread_id, temp);
 }
 
+//poprawne funkcje
 void *reader_thread(void *arg){
-    pthread_t thread_id = thead_self();
+    pthread_t thread_id = pthread_self();
     printf("{%d} Start readed\n", thread_id);
     pthread_rwlock_rdlock(&counter_lock);
     int temp = counter;
     sleep(1);
-    pthread_rwlock_rdlock(&counter_lock);
+    pthread_rwlock_unlock(&counter_lock);
     printf("{%d} Read %d\n", thread_id, temp);
 }
 
 void *writer_thread(void *arg){
-    pthread_t thread_id = thead_self();
-    printf ("{%d} Start writing")
+    pthread_t thread_id = pthread_self();
+    printf ("{%d} Start writing\n", thread_id);
+    pthread_rwlock_wrlock(&counter_lock);
+    int temp = counter;
+    temp += 1;
+    sleep(1);
+    counter = temp;
+    pthread_rwlock_unlock(&counter_lock);
+    printf("{%d} Write %d\n", thread_id, temp);
 }
 
 int main(int argc, char* argv[]) {
@@ -53,21 +63,21 @@ int main(int argc, char* argv[]) {
 
     for (int i = 0; i < THREAD_COUNT; i++) {
         int result;
-        if ((i == 4) || (i == 12)) {
-          result = pthread_create(&(threads[i]), NULL, writer_thread, NULL);
-        } else {
-          result = pthread_create(&(threads[i]), NULL, reader_thread, NULL);
+        if((i % 2 == 0)){
+            result = pthread_create(&(threads[i]), NULL, writer_thread, NULL);
+        }else{
+            result = pthread_create(&(threads[i]), NULL, reader_thread, NULL);
         }
         if (result != 0) {
-            perror("Could not create thread.");
+            perror("Could not create thread.\n");
         }
     }
 
-    for (int i = 0; i < THREAD_COUNT; i++) {
+    for (int i = 1; i < THREAD_COUNT; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    pthread_mutex_destroy(&counter_lock);
+    pthread_rwlock_destroy(&counter_lock);
 
     printf("\nCounter value after executing %d threads is %d.\n", THREAD_COUNT, counter);
     return 0;
